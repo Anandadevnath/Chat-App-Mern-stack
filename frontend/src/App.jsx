1fa7097613cb96079a6a8bdfa318e2f59892b56f
Ananda Dev Nath <thunderhorizon18@gmail.com>
2024-12-24 22:39:59 +0600

    Frontend Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css';
-import Login from "./pages/login/login";
+import Home from './pages/home/Home';
 
 function App() {
   return (
-  <div className='p-4 h-screen flex items-center justify-center'>
-    <Login />
-  </div>
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <Home />
+    </div>
   );
 }
-
 export default App;
