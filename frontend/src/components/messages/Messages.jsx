e09a960e04d2cbf89008cd729aa7b5f0343e3cb1
Ananda Dev Nath <thunderhorizon18@gmail.com>
2024-12-29 00:28:40 +0600

    finished

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -2,9 +2,11 @@ import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
+import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
 	const { messages, loading } = useGetMessages();
+	useListenMessages();
 	const lastMessageRef = useRef();
 
 	useEffect(() => {
@@ -15,9 +17,13 @@ const Messages = () => {
 
 	return (
 		<div className='px-4 flex-1 overflow-auto'>
-			{!loading && messages.length > 0 && messages.map((message) => (
-				<Message key={message._id} message={message} />
-			))}
+			{!loading &&
+				messages.length > 0 &&
+				messages.map((message) => (
+					<div key={message._id} ref={lastMessageRef}>
+						<Message message={message} />
+					</div>
+				))}
 
 			{loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 			{!loading && messages.length === 0 && (
