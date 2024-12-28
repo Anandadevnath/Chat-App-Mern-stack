408e9b65fd8b105089db60f10a2558b0ba4241f0
Ananda Dev Nath <thunderhorizon18@gmail.com>
2024-12-29 00:48:16 +0600

    deploying

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,20 @@
-import express from "express"; 
-import dotenv from "dotenv"; 
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
-import authRoutes from "./routes/auth.routes.js"; 
-import messageRoutes from "./routes/message.routes.js"; 
+import path from "path";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-import connectToMongoDB from "./db/connectToMongodb.js";  
+import connectToMongoDB from "./db/connectToMongodb.js";
 import { app, server } from "./socket/socket.js";
 
 
-const PORT = process.env.PORT || 3000;   
+const PORT = process.env.PORT || 3000;
 
-dotenv.config(); 
+const __dirname = path.resolve();
+
+dotenv.config();
 
 app.use(express.json()); // this will allow us to extract the req from authRoutes.controller.js
 app.use(cookieParser()); // this will allow us to extract the req from authRoutes.controller.js
@@ -21,14 +23,20 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 app.get("/", (req, res) => {
-   // root route http://localhost:3000/
-   res.send("server is ready");  
+    // root route http://localhost:3000/
+    res.send("server is ready");
 });
 
 
 // Adding listener to the server to run the server on a specific port
-server.listen(PORT, () => { 
-    connectToMongoDB(); 
-    console.log(`Server is running on port ${PORT}`);  
+server.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server is running on port ${PORT}`);
 });   
